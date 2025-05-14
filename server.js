7f0b62bc6e9cb62afb3d12a69d15e5ea3d0ebe24
Hagar Elmadany <hagarelmadany@gmail.com>
2025-05-14 15:34:34 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express =require('express');
+const mongoose =require('mongoose');
+const cors =require('cors');
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUNifiedTopology: true,
+})
+.then(()=> {console.log('MongoDB connected')})
+.catch((err)=> {console.error('MongoDB connection error:', err)});
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('server is working');
+})
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
