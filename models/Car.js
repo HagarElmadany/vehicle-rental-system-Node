6b8aa432918546c4cfd460ddd9f85a1fa5a71230
NadaMohamed2243 <106352195+NadaMohamed2243@users.noreply.github.com>
2025-05-15 20:17:17 +0300

    (models): add Car and Exhibition schemas

---
+++ models/Car.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    brand: { type: String, required: true }, //-
+    model: { type: String, required: true }, //-
+    type: { type: String, required: true },  //-
+    year: { type: Number, required: true }, //-
+    licensePlate: { type: String, required: true, unique: true }, //-
+    // vin: { type: String, required: true, unique: true }, 
+    transmission: { type: String, enum: ['Automatic', 'Manual'], required: true }, //-
+    fuel_type: { type: String, enum: ['Petrol', 'Diesel', 'Electric', 'Hybrid'], required: true }, //-
+    seats: { type: Number, min: 2, max: 8, required: true },  //-
+    color: { type: String, required: true }, //-
+    // mileage: { type: Number, required: true },
+    rentalRatePerDay: { type: Number , default:40 },
+    rentalRatePerHour: { type: Number ,default:10},
+    availabilityStatus: { type: String, enum: ['Available', 'Rented', 'Under Maintenance'], default: 'Available' }, //-
+    rating: { type: Number, min: 0, max: 5, default: 4.5 }, //-
+    depositRequired: { type: Number, default:100 }, //-
+    insuranceStatus: { type: String, enum: ['Full Coverage', 'Partial Coverage', 'No Coverage'], default: 'Full Coverage' },
+    lastMaintenanceDate: { type: Date },
+    nextMaintenanceDue: { type: Date },
+    conditionNotes: { type: String },
+    fuelLevel: { type: String, enum: ['Empty', 'Quarter', 'Half', 'Three-Quarters', 'Full'], default: 'Full' }, //-
+    odometerReading: { type: Number, required: true }, //-
+    lastRentedDate: { type: Date },
+    expectedReturnDate: { type: Date },
+    totalPricePerHour: { type: Number ,required: true}, //-
+    totalPricePerDay: { type: Number ,required: true}, //-
+    carPhotos: [{ type: String }],
+      exhibition: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Exhibition',
+        required: true 
+      },
+}, { timestamps: true });
+
+const Car = mongoose.model('Car', carSchema);
+
+module.exports = Car;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 //connect to MongoDB
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
-    useUNifiedTopology: true,
+    useUnifiedTopology: true,
 })
 .then(()=> {console.log('MongoDB connected')})
 .catch((err)=> {console.error('MongoDB connection error:', err)});
