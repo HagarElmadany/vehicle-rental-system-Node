09a6d471a4fd9d5ccc0ff7ee656ab01fedb0224e
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-05-31 22:22:54 +0300

    commit changes since last time

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
-const express =require('express');
-const mongoose =require('mongoose');
-const cors =require('cors');
-const dotenv = require('dotenv');
-dotenv.config(); // Load environment variables from .env file
-
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
 const app = express();
-const PORT= process.env.PORT || 5000;
-
-
-//Middleware
-app.use(cors());
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ MongoDB error:', err));
 
-//connect to MongoDB
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUNifiedTopology: true,
-})
-.then(()=> {console.log('MongoDB connected')})
-.catch((err)=> {console.error('MongoDB connection error:', err)});
+app.use('/api/auth', authRoutes);
 
-
-//routes
-app.get('/', (req, res) => {
-    res.send('server is working');
-})
-
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
 });
