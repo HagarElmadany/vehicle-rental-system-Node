c3b389f8a2d7a5eec1ddea1caf2daf7d264805af
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-06-01 00:40:41 +0300

    create users schemas , including USER,ADMIN,AGENT,CLIENT

---
+++ models/Client.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  first_name: { type: String, required: true, maxlength: 100 },
+  last_name: { type: String, required: true,  maxlength: 100 },
+  phone_number: { type: String, required: true, maxlength: 15 },
+  location: { type: String, required: true, maxlength: 100 },
+  lat: { type: Number},
+  lng: { type: Number},
+  driver_license: { type: String, maxlength: 255 },
+  verification_status: {
+    type: String,
+    enum: ['approved', 'pending', 'banned','rejected','suspended'],
+    default: 'pending'
+  },
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Car' }]
+});
+
+module.exports = mongoose.model('Client', clientSchema);
+
+
+/* explanation of verification status
+
+approved
+The client’s verification process is complete and successful. They are fully authorized and allowed to use the platform without restrictions.
+
+pending
+The client’s verification is still under review or waiting to be completed. They may have limited access until verification finishes.
+
+banned
+The client is prohibited from accessing the platform due to violations, fraud, or other serious issues. This is permenant.
+
+rejected
+The client’s verification request was reviewed and denied. They did not meet the necessary criteria to be verified and may need to reapply or fix issues.
+
+suspended
+The client’s access is temporarily disabled, often due to suspicious activity or pending investigation. Suspension is usually reversible after conditions are met or time passes.
+
+*/
--- models/User.js
+++ models/User.js
@@ -1,45 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required'],
-        minlength: [2, 'Name must be at least 2 characters long'],
-        maxlength: [50, 'Name cannot exceed 50 characters']
-    },
     email: {
         type: String,
         required: [true, 'Email is required'],
         unique: true,
         lowercase: true,
-        match: [/\S+@\S+\.\S+/, 'Email is invalid']
+        match: [/\S+@\S+\.\S+/, 'Email is invalid'],
+        trim: true
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
         minlength: [6, 'Password must be at least 6 characters']
     },
-    phone: {
-        type: String,
-        required: [true, 'Phone number is required'],
-        match: [/^01[0-9]{9}$/, 'Phone must be a valid Egyptian number (e.g. 010xxxxxxxx)']
-    },
-    location: {
-        type: String,
-        trim: true,
-        required: [true, 'Location is required'],
-        maxlength: [100, 'Location cannot exceed 100 characters']
-    },
-    role: {
-        type: String,
-        enum: ['admin', 'user'],
-        default: 'user'
-    },
-    licenseImage: String,
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    verified: { type: Boolean, default: false },
-    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle' }]
+    role: { type: String, enum: ['admin', 'agent', 'client'], default: 'client', required: true },
+},
+{
+    timestamps: true,
 });
 
 module.exports = mongoose.model('User', userSchema);
