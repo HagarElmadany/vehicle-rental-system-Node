3ec9085225e6e3545411264d58e930cea316e52b
nouranmostafa-cell <nouranmostafa1234@gmail.com>
2025-06-11 23:56:39 +0300

    fix a bug in google auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const upload = require('../middleware/upload');
 const verifyToken = require('../middleware/verifyToken');
 const authController = require('../controllers/AuthController');
+const passport = require('passport');
 
 // Register routes
 router.post('/register/client', upload.single('driver_license'), (req, res) => authController.registerUser(req, res, 'client'));
@@ -20,10 +21,27 @@ router.post('/reset-password/:token', authController.resetPassword);
 // Get client profile
 router.get('/client/profile', verifyToken, authController.getClientProfile);
 
-// Google OAuth login
-router.post('/google/login', authController.googleOAuthLogin);
 
-// Complete Google profile (after login)
-router.post('/google/complete-profile', verifyToken, authController.completeGoogleProfile);
+router.get('/google', (req, res, next) => {
+  const role = req.query.role; // 'client' or 'agent'
+  const state = JSON.stringify({ role });
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    state
+  })(req, res, next);
+});
+
+// Google OAuth2 callback
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  authController.googleCallback
+);
+
+// Complete Google Profile
+router.post('/google/complete-profile',
+  verifyToken,
+  upload.single('driver_license'),
+  authController.completeGoogleProfile
+);
 
 module.exports = router;
